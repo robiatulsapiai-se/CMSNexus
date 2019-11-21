98984815aadcdd3233949b66ed230705279aac33
robiatul sapia'i <57268887+robiatulsapiai-se@users.noreply.github.com>
2019-11-21 15:24:26 +0800

    Update about.php

--- about.php
+++ about.php
@@ -7,8 +7,6 @@
 <link rel="stylesheet" type="text/css" href="w3css1.css">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-
 <body class="w3-light-grey w3-content" style="max-width:1600px">
 
 <!-- Sidebar/menu -->
